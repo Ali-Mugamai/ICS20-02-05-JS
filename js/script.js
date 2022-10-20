e9441cba12da8b8d8f5c7b39b48436c56f8815ff
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-20 13:24:54 -0400

    pleae be in the clan

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Ali Mugamai All rights reserved
+//
+// Created by: Ali Mugamai
+// Created on: oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate() {
+  // input
+  const hours = parseInt(document.getElementById("hours_worked").value)
+  const rate = parseInt(document.getElementById("hourly_rate").value)
+  // process
+  const pay = (hours * rate) * (1.00 * 0.18)
+  // output
+  document.getElementById("pay").innerHTML = "your pay is: " + pay
+}
