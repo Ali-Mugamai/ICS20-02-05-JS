0ce3ae25532b222dfda02cb8e4454a14cacc5bd4
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-20 17:30:50 -0400

    koo

--- js/script.js
+++ js/script.js
@@ -13,7 +13,9 @@ function calculate() {
   const hours = parseInt(document.getElementById("hours_worked").value)
   const rate = parseInt(document.getElementById("hourly_rate").value)
   // process
-  const pay = hours * rate * (1.0 * 0.18)
+  const pay = (hours * rate) * (1.00 * 0.18)
+  const taxes = (hours * rate) * 0.18
   // output
   document.getElementById("pay").innerHTML = "your pay is: " + pay
+  document.getElementById("governmentstake").innerHTML = "the government will take " + taxes
 }
